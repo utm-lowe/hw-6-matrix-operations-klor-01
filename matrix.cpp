f7cc4eb4fcb3103225d1e819eca4bd19ab63c3b9
Kimsear Lor <klor@ut.utm.edu>
2026-03-04 15:53:12 +0000

    change spaceing

--- matrix.cpp
+++ matrix.cpp
@@ -122,7 +122,7 @@ std::ostream& operator<<(std::ostream & os, const Matrix& m){
         for(int j = 0; j<m.getCols(); j++){
             os << std::setw(10) << m.at(i,j) << " ";
         }
-        os << " " << std::endl;
+        os << "" << std::endl;
     }
     return os;
 
